eba2c0449d65d06049a6a3b77b7982339581f63c
Shippo704 <matt.taylor704@yahoo.ca>
2023-10-08 16:54:58 -0400

    added logic to putDb method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,32 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  try {
+    // connect to db
+    const connectDB = await openDB('jate', 1);
+
+    // create new transaction and define privileges
+    const newTx = connectDB.transaction('jate', 'readwrite');
+
+    // open the object store
+    const store = tx.objectStore('jate');
+
+    // make the PUT request
+    const putRequest = store.put({id: 1, value: content});
+
+    // get result status
+    const result = await putRequest;
+    console.log('data saved: ', result);
+  }
+  // catch errors
+  catch (error) {
+    console.error('putDb not implemented');
+  } 
+};
+
+
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
