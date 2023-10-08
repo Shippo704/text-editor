369f75c1f456fd90b3f933bf7bea2ba2c1399a11
Shippo704 <matt.taylor704@yahoo.ca>
2023-10-08 16:20:16 -0400

    added plugins to webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+// add plugin for CSS
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -18,7 +20,38 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // html webpack plugin
+      new HtmlWebpackPlugin({
+        template: '.index.html',
+        title: 'text-editor'
+      }),
+      // css webpack plugin
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFileName: '[id].css'
+      }),
+      // manifest plugin
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+      // pws manifest plugin
+      new WebpackPwaManifest({
+        name: 'text-editor',
+        short_name: 'text-editor',
+        description: 'This is a text editor',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
