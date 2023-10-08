b91abf4baf5c1e771743cc14a9d3617a989fd745
Shippo704 <matt.taylor704@yahoo.ca>
2023-10-08 17:09:34 -0400

    added functionality to install button

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,34 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    // store event for later
+    window.deferredPrompt = event;
+    // make install button visible if app has not been installed yet
+    butInstall.classList.toggle('hidden', false);
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    // retrieve old events
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        return;
+    }
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+    // show prompt
+    promptEvent.prompt();
+
+    // reset deferredPrompt so it can be used again
+    window.deferredPrompt = null;
+
+    // hide install button after clicking it
+    butInstall.classList.toggle('hidden', true);
+});
+
+// Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    // reset deferredPrompt
+    window.deferredPrompt = null;
+});
